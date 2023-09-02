6e9342902b0cae8c67810a8f1f3cca8d321de621
Saidkamolkhuja <iamkhuja@mail.ru>
2023-09-02 10:12:16 +0500

    Section 4 - N-Tier Architecture

--- BulkyWeb/Controllers/CategoryController.cs
+++ BulkyWeb/Controllers/CategoryController.cs
@@ -1,8 +1,8 @@
-﻿using BulkyWeb.Data;
-using BulkyWeb.Models;
+﻿using Bulky.DataAccess.Data;
+using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 
-namespace BulkyWeb.Controllers
+namespace Bulky.Web.Controllers
 {
     public class CategoryController : Controller
     {
--- BulkyWeb/Controllers/HomeController.cs
+++ BulkyWeb/Controllers/HomeController.cs
@@ -1,8 +1,8 @@
-using BulkyWeb.Models;
+using Bulky.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
-namespace BulkyWeb.Controllers
+namespace Bulky.Web.Controllers
 {
     public class HomeController : Controller
     {
